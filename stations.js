3471e202949d1e4a34d7bdfdad79ee7e1b1c5a13
Asim75772 <146651181+Asim75772@users.noreply.github.com>
2026-08-28 12:57:17 +0530

    Create stations.js

---
+++ stations.js
@@ -0,0 +1,102 @@
+const stations = [
+
+  {
+    name: "Radio Sharanam",
+    language: "Bengali",
+    category: "Bengali Bhakti",
+    icon: "🪔",
+    stream: "",
+    website: "https://www.jawaradio.com/"
+  },
+
+  {
+    name: "Kripalu Bhakti Dhara",
+    language: "Hindi",
+    category: "Bhajan & Spiritual",
+    icon: "🙏",
+    stream: "",
+    website: "https://radio.jkp.org.in/"
+  },
+
+  {
+    name: "JKYog Radio",
+    language: "Hindi",
+    category: "Bhajan & Kirtan",
+    icon: "🕉️",
+    stream: "",
+    website: "https://www.jkyog.org/"
+  },
+
+  {
+    name: "Bhaktiworld Shiva",
+    language: "Hindi",
+    category: "Shiva",
+    icon: "🔱",
+    stream: "",
+    website: "https://www.bhaktiworld.com/"
+  },
+
+  {
+    name: "Bhaktiworld Krishna",
+    language: "Hindi",
+    category: "Krishna",
+    icon: "🪷",
+    stream: "",
+    website: "https://www.bhaktiworld.com/"
+  },
+
+  {
+    name: "Bhaktiworld Devi Maa",
+    language: "Hindi",
+    category: "Devi",
+    icon: "🌺",
+    stream: "",
+    website: "https://www.bhaktiworld.com/"
+  },
+
+  {
+    name: "Bhaktiworld Ganesha",
+    language: "Hindi",
+    category: "Ganesha",
+    icon: "🐘",
+    stream: "",
+    website: "https://www.bhaktiworld.com/"
+  },
+
+  {
+    name: "Bhaktiworld Hanuman Ji",
+    language: "Hindi",
+    category: "Hanuman",
+    icon: "🚩",
+    stream: "",
+    website: "https://www.bhaktiworld.com/"
+  },
+
+  {
+    name: "Shirdi Sai Bhakti Radio",
+    language: "Hindi",
+    category: "Sai Baba",
+    icon: "🙏",
+    stream: "",
+    website: "https://www.saibhaktiradio.com/"
+  },
+
+  {
+    name: "Swaminarayan Kirtan Radio",
+    language: "Devotional",
+    category: "Kirtan",
+    icon: "🎵",
+    stream: "",
+    website: "https://swaminarayan.me/"
+  },
+
+  {
+    name: "Radio Vedic",
+    language: "Hindi",
+    category: "Vedic & Mantra",
+    icon: "📿",
+    stream: "",
+    website: "https://play.google.com/"
+  }
+
+];
